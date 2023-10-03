2b63626cc2f8f68bfdb777c5fd8db75f42144b34
Zhenya Osokina <kapitan.kub@gmail.com>
2023-10-03 13:18:48 +0500

    фикстуры задание 6

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,16 +1,23 @@
 import unittest
+
+import pytest
+
 from utils import arrs
 
 
-class TestArrs(unittest.TestCase):
+@pytest.fixture
+def arr_():
+    return [1, 2, 3, 4, 5]
 
-    def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
 
-    def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
-        self.assertEqual(arrs.my_slice([]),[])
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], 1),[2, 3, 4, 5])
-        self.assertEqual(arrs.my_slice([2, 3], None, 3), [2, 3])
+def test_get(arr_):
+    assert arrs.get(arr_, 1, "test") == 2
+    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get(arr_, 6, "hi_test") == "hi_test"
+
+def test_slice(arr_):
+    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], 1) == [2, 3, 4, 5]
+    assert arrs.my_slice([2, 3], None, 3) == [2, 3]
